98dc7aa4ac5fd8ab7757f8606e8225a078988acf
HipsterSantos <santoscampos269@gmail.com>
2024-07-16 12:06:19 +0100

    [chores] adding receive other player event

--- main.js
+++ main.js
@@ -9,6 +9,33 @@ window.addEventListener("DOMContentLoaded", () => {
   sendMoves(board, websocket);
 });
 
+function showMessages(message){
+    setTimeout(()=>alert(message),50)
+}
+
+function receiveMoves(board,websocket){
+    websocket.addEventListener('message',({data})=>{
+        const event = JSON.parse(data)
+        switch(data.type){
+            case 'play': {
+                playMove(board,event.player,event.column,event.row)
+                break;
+            }
+            case 'win': {
+                showMessages(`Player ${event.player} have won `)
+                websocket.close(1000);
+                break;
+            }
+            case 'error': {
+                showMessages(event.message)    
+                break;
+            }
+            default: 
+                throw new Error('Unsupported event type ')
+        }
+    })
+}
+
 function sendMoves(board,websocket){
     board.addEventListener('click',({target})=>{
         const column = target.dataset.column
